a646ff8e8231560c0898acb9ef1979ac65ce0726
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2024-01-27 18:54:49 -0500

    Add nutrition request to api

--- backend/food_classifier.py
+++ backend/food_classifier.py
@@ -6,20 +6,23 @@ import pandas as pd
 import cv2
 from skimage import io
 import os
+import requests
+from app import key
 
 root_dir = os.path.dirname(__file__)
 labelmap_dir = os.path.join(root_dir, "model/aiy_food_V1_labelmap.csv")
 model_dir = os.path.join(root_dir, "model/")
 # images_dir = os.path.
 
-# model = hub.KerasLayer('https://www.kaggle.com/models/google/aiy/frameworks/TensorFlow1/variations/vision-classifier-food-v1/versions/1')
 model = hub.KerasLayer(model_dir)
 
-test_url = "https://www.allrecipes.com/thmb/5JVfA7MxfTUPfRerQMdF-nGKsLY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg"
+test_url = "https://d3mvlb3hz2g78.cloudfront.net/wp-content/uploads/2014/06/thumb_720_450_dreamstime_xl_34122178-Custom.jpg"
 input_shape = (224, 224)
 
 # Food classification model from https://www.kaggle.com/models/google/aiy/frameworks/tensorFlow1/variations/vision-classifier-food-v1
 
+
+model_label_map_classes = list(pd.read_csv(labelmap_dir)["name"])
 def PredictImage(image_path):
     image = np.asarray(io.imread(image_path), dtype="float")
     image = cv2.resize(image, dsize=input_shape, interpolation=cv2.INTER_CUBIC)
@@ -30,10 +33,26 @@ def PredictImage(image_path):
     # This assumes you're using TF2.
     output = model(images)
     predicted_index = output.numpy().argmax()
-    classes = list(pd.read_csv(labelmap_dir)["name"])
-    print("Prediction: ", classes[predicted_index])
+    print("Prediction: ", model_label_map_classes[predicted_index])
+    return model_label_map_classes[predicted_index]
 
-PredictImage(test_url)
+def RequestNutrition(food_item):
+    response = requests.get(f'https://api.edamam.com/api/nutrition-data?app_id=8997459b&app_key={key}&nutrition-type=logging&ingr={food_item}').json()
+
+    output = {
+        "name" : food_item,
+        "calories" : response['calories'],
+        "protein": response['totalNutrients']['PROCNT']['quantity'],
+        "carbohydrates": response['totalNutrients']['CHOCDF.net']['quantity'],
+        "fat": response['totalNutrients']['FAT']['quantity'],
+        "sugar": response['totalNutrients']['SUGAR']['quantity'],
+        "carbon": response['totalCO2Emissions'],
+        "carbon_class": response['co2EmissionsClass']
+    }
+    return output
+
     
-# def get_nutrition(food):
+prediciton = PredictImage(test_url)
+proper_json = RequestNutrition(prediciton)
+print(proper_json)
     
