5484ecf75d0aaf446c8d414fa8debf8ce6474b26
jobin-b <jobinbabu.hpj@gmail.com>
2024-01-27 17:01:53 -0500

    add auth0

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "dotenv/config";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={process.env.AUTH0_DOMAIN ?? ""}
+    clientId={process.env.AUTH0_CLIENT_ID ?? ""}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginButton from "../components/login";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <LoginButton />
+    </div>
+  );
+}
